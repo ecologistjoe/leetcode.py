d8c93edf21154d92a8bb49de848d54645a8f6b4f
ecologistjoe <m.joseph.hughes@gmail.com>
2022-02-03 16:17:57 -0500

    pretty ugly right now, but it works!

--- largestTriangleArea.py
+++ largestTriangleArea.py
@@ -3,10 +3,14 @@
 # the naive O(n^3) solution is what was expected
 
 # Area of a Triangle: 1/2*(x1*y2 + x2*y3 + x3*y1 - y1*x2 - y2*x3 -y3*x1)
+import matplotlib.pyplot as plt
+import random
+from time import time
 
 class Solution:
 
     # The brute-force search O(n^3) solution
+    # There's probably small pythonic optimizations that could be done
     def largestTriangleAreaO3(self, points):
         N = len(points)
         maxA = -2**31
@@ -44,6 +48,7 @@ class Solution:
         # Some points may be the same. That's ok
         # Build a polygon with points in anti-clockwise order, starting with
         # the left-most point. The start is arbitrary, but the order is important
+
         P = [[0,0] for i in range(8)]
         for p in points:
             x,y = p
@@ -55,9 +60,10 @@ class Solution:
             if x+y > maxS: maxS, P[5] = x+y, p
             if y   > maxY: maxY, P[6] = y, p
             if y-x > maxD: maxD, P[7] = y-x, p
-        
+                
+        #Collect into a list of unique vertices
         extrema = []
-        for i in range(0,8):
+        for i in range(8):
             if P[i] not in extrema:
                 extrema += [P[i]]
         
@@ -71,7 +77,7 @@ class Solution:
         #Get vertices of an up-to-8-sided polygon where the vertices
         # are extrema from the pointcloud
         extrema = self.extremePoints(points)
-        
+
         # Loop through all of the points. We'll check and see if they are
         # outside of the polygon defined by extrema. In most datasets,
         # only about sqrt(N) of the points will be outside of the polygon
@@ -82,48 +88,98 @@ class Solution:
         # The equation determines what side of the line defined by the consecutive
         # vertices any given point is. Since we're traversing the vertices strictly
         # counter-clockwise, we only want to keep ones on the 'right'
+        
         N = len(extrema)
-        Buffer = [ [extrema[j]] for j in range(N)]
+        Buffer = [[] for j in range(N)]
+        deltaX = [extrema[j-1][0]-extrema[j][0] for j in range(N)]
+        deltaY = [extrema[j-1][1]-extrema[j][1] for j in range(N)]
+        
         for p in points:
             pX,pY = p
             for j in range(N):
-                x1,y1 = extrema[j-1]
-                x2,y2 = extrema[j]
-                if (x2-x1)*(pY-y1) < (y2-y1)*(pX-x1):               
-                    Buffer[j] += [p]
+                x1,y1 = extrema[j]
+                if deltaX[j]*(pY-y1) > deltaY[j]*(pX-x1):
+                    Buffer[j].append(p)
+                    break
         
-        # For each side, we'll add those outside points to our hull or
-        # calculate its own hull, and then add those points (recursing as needed) 
+        outside =[p for B in Buffer for p in B]
+
         hull = []
         for j in range(N):
-            if len(Buffer[j]) > 2:
-                # There's 'many' (to a gully dwarf) points in this buffer
-                # so we'll simplify it by finding its own complex hull
-                Buffer[j] = self.convexHull(Buffer[j])
-            # There's only the vertex itself or it plus one other point,
-            # which must therefore be on the hull as well. Collect them
-            hull += Buffer[j]
+            # This proposed improvement -- iteratively weeding out the points
+            # doesn't do much. Only about a 2% improvement
+            # Almost all of the work is done in the initial outside/inside determination
+            #if len(Buffer[j])>len(points)**0.5:
+            #    Buffer[j],dummy,dummy = self.convexHull(Buffer[j])
+            
+            hull+= [extrema[j-1]]
+            x1,y1 = hull[-1]
+            x3,y3 = extrema[j]
+            for x2,y2 in Buffer[j]:
+                Buffer[j] = [[pX,pY]
+                    for pX,pY in Buffer[j]
+                    if (x2==pX and y2==pY) or 
+                        (((x2-x1)*(pY-y1) < (y2-y1)*(pX-x1)) or
+                         ((x3-x2)*(pY-y2) < (y3-y2)*(pX-x2)))
+                    ]
+                
+            slopes = []
+            for pX,pY in Buffer[j]:
+                if pX==x1:
+                    slopes += [float('inf')]
+                else:
+                    slopes += [(pY-y1)/(pX-x1)]
+            
+            hull += [p for s,p in sorted(zip(slopes, Buffer[j]), key=lambda pair: pair[0])]
+
         
-        return hull
+        return hull, extrema, outside
                     
 
     def largestTriangleArea(self, points):
         # Step one, find a polygon that encloses all points.
         # The biggest triangle must be made from these points
-        hull = self.convexHull(points)
+        hull,_,_= self.convexHull(points)
         
         # Use the brute force algorithm to find the area from the much smaller
         # number of hull points.
         maxArea = self.largestTriangleAreaO3(list(hull))
-        
-        
         return maxArea
         
-
-
+        
 S = Solution()
 points = [[0,0],[0,1],[0,2],[1,0],[1,1],[2,0]]
-points = [[42, 1], [34, 49], [2, 5], [33, 30], [8, 37], [43, 9], [38, 37], [23, 8], [48, 30], [11, 25], [16, 20], [22, 44], [40, 42], [36, 16], [47, 46], [29, 34], [34, 6], [20, 9], [40, 38], [24, 10], [24, 12], [44, 30], [36, 2], [34, 27], [39, 28], [45, 23], [15, 28], [26, 30], [31, 22], [3, 5], [41, 20], [50, 42], [43, 47], [25, 19], [19, 48], [41, 47], [12, 11], [32, 44], [28, 40], [49, 2]]
-points = [[13,-50],[43,3],[-35,38],[-9,-14],[9,2],[43,-3],[-1,-32],[25,7],[28,4],[26,16]]
 
-print(S.largestTriangleArea(points))
+points = [[42, 1], [34, 49], [2, 5], [33, 30], [8, 37], [43, 9], [38, 37], [23, 8], [48, 30], [11, 25], [16, 20], [22, 44], [40, 42], [36, 16], [47, 46], [29, 34], [34, 6], [20, 9], [40, 38], [24, 10], [24, 12], [44, 30], [36, 2], [34, 27], [39, 28], [45, 23], [15, 28], [26, 30], [31, 22], [3, 5], [41, 20], [50, 42], [43, 47], [25, 19], [19, 48], [41, 47], [12, 11], [32, 44], [28, 40], [49, 2]]
+
+random.seed(0)
+x =  [random.triangular(0, 1, 0.5) for i in range(int(1e6))]
+y =  [random.gauss(0, 1) for i in range(int(1e6))]
+
+points = [[a,b] for a,b in zip(x,y)]
+#points = [[13,-50],[43,3],[-35,38],[-9,-14],[9,2],[43,-3],[-1,-32],[25,7],[28,4],[26,16]]
+#x,y = zip(*points)
+
+tic = time()
+hull,extrema,outside = S.convexHull(points)
+toc = time()
+print(toc-tic)
+
+
+fig,ax= plt.subplots()
+ax.scatter(x,y)
+
+outsidex, outsidey = zip(*outside)
+ax.scatter(outsidex,outsidey, color='green')
+
+hull += [hull[0]]
+hullx, hully = zip(*hull)
+ax.plot(hullx,hully, color='orange')
+ax.scatter(hullx,hully, color='orange')
+plt.show()
+
+#print(S.largestTriangleArea(points))
+
+
+
+
